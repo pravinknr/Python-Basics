f00f6ddb647d66a0c8bc49a0ee3d6b25ce4d2604
Pravin Konar <pravinknr@gmail.com>
2020-06-12 22:06:32 +0530

    Added Examples

---
+++ 06. Examples/01. Print_Your_Name.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+# A very Simple Function/ Program
+
+# Lets Print your Own Name.
+print('Pravin Konar')
+
+# Lets Ask the user to Enter the Name.
+input("What is Your Name?")
+
+# Lets Store it in a Variable
+Name = input("What is your name?")
+print(Name)
+
+print("Hello " + Name)
+
